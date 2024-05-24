db2255f4b697d5f0dd19e1d1a2cb9a46feed9d7e
Luiz Henrique Cassettari <ricaun@gmail.com>
2024-05-24 15:45:00 -0300

    Update Readme

--- README.md
+++ README.md
@@ -6,7 +6,26 @@
 
 RevitTest project to test workaround to create Detail Groups in Revit without ActiveView.
 
-Revit API Forum: https://forums.autodesk.com/t5/revit-api-forum/placing-detail-group-in-legend-view-without-access-to-ui-design/td-p/12792753
+This project use the [ricaun.RevitTest](https://ricaun.com/RevitTest) test framework.
+
+## Problem Description
+
+Project is based on the problem described in the [Revit API Forum](https://forums.autodesk.com/t5/revit-api-forum/placing-detail-group-in-legend-view-without-access-to-ui-design/td-p/12792753).
+
+The method `PlaceGroup` place the Group detail in the `ActiveView`, but the `ActiveView` is not available the `Group` is created in an invalid view (`OwnerViewId` is `ElementId.InvalidElementId`).
+
+A workaround is to use a `Group` in the document and copy to the view you want, and change the type of the `Group`, than change the location.
+
+The method below does this workaround:
+```c#
+public Group CreateGroupDetail(GroupType groupType, View view, XYZ location);
+```
+
+## Video
+
+Videos in English about this project.
+
+[![VideoIma1]][Video1]
 
 ## License
 
@@ -15,3 +34,6 @@ This project is [licensed](LICENSE) under the [MIT License](https://en.wikipedia
 ---
 
 Do you like this project? Please [star this project on GitHub](../../stargazers)!
+
+[Video1]: https://youtu.be/VhaFv_UaHGU
+[VideoIma1]: https://img.youtube.com/vi/VhaFv_UaHGU/mqdefault.jpg
--- RevitTestPlaceGroup.Tests/GroupDetailService.cs
+++ RevitTestPlaceGroup.Tests/GroupDetailService.cs
@@ -1,88 +1,10 @@
 ﻿using Autodesk.Revit.DB;
-using NUnit.Framework;
-using RevitTestPlaceGroup.Tests.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace RevitTestPlaceGroup.Tests
 {
-    public class Tests : OneTimeOpenDocumentTest
-    {
-        GroupDetailService groupDetailService = new GroupDetailService();
-        protected override string FileName => "Files/ProjectGroupDetail2021.rvt";
-
-        [Test]
-        public void Test_Document_Save()
-        {
-            Assert.IsNotNull(document);
-            Console.WriteLine(document.Title);
-
-            document.SaveAs($"Files/{document.Title}_{application.VersionNumber}.rvt", new SaveAsOptions() { MaximumBackups = 1, OverwriteExistingFile = true });
-        }
-
-        [Test]
-        public void PlaceGroupDetail_ShouldBe_InvalidElementId()
-        {
-            var groupType = groupDetailService.SelectType(document).FirstOrDefault();
-            Assert.IsNotNull(groupType);
-
-            using (Transaction transaction = new Transaction(document))
-            {
-                transaction.Start("PlaceGroup");
-                var group = document.Create.PlaceGroup(new XYZ(0, 0, 0), groupType);
-
-                Assert.IsNotNull(group);
-                Assert.AreEqual(ElementId.InvalidElementId, group.OwnerViewId);
-
-                transaction.Commit();
-            }
-
-        }
-
-        [Test]
-        public void CreateGroupDetail_ShouldBe_ValidElementId()
-        {
-            var viewLegend = SelectViewLegend().FirstOrDefault();
-
-            Assert.IsNotNull(viewLegend);
-
-            var groupTypes = groupDetailService.SelectType(document);
-
-            foreach (var groupType in groupTypes)
-            {
-                using (Transaction transaction = new Transaction(document))
-                {
-                    transaction.Start("PlaceGroup");
-                    var group = groupDetailService.CreateGroupDetail(groupType, viewLegend, new XYZ(0, 0, 0));
-
-                    Assert.IsNotNull(group);
-                    Assert.AreEqual(viewLegend.Id, group.OwnerViewId);
-
-                    transaction.Commit();
-                }
-            }
-        }
-
-        private IEnumerable<View> SelectViewLegend()
-        {
-            return new FilteredElementCollector(document)
-                .OfClass(typeof(View))
-                .OfType<View>()
-                .Where(e => e.ViewType == ViewType.Legend);
-        }
-
-        [Test]
-        public void ShowGroups()
-        {
-            foreach (var groupType in groupDetailService.SelectType(document))
-            {
-                Console.WriteLine(groupType.Name);
-            }
-        }
-
-    }
-
     public class GroupDetailService
     {
         public IEnumerable<GroupType> SelectType(Document document)
