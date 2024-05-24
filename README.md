b5cb83766f3732fb08f63342a7391d79922c58af
Luiz Henrique Cassettari <ricaun@gmail.com>
2024-05-24 15:11:35 -0300

    Add project files.

---
+++ README.md
@@ -0,0 +1,17 @@
+﻿# RevitTestPlaceGroup.Tests
+
+[![Revit 2021](https://img.shields.io/badge/Revit-2021+-blue.svg)](../..)
+[![Visual Studio 2022](https://img.shields.io/badge/Visual%20Studio-2022-blue)](../..)
+[![License MIT](https://img.shields.io/badge/License-MIT-blue.svg)](LICENSE)
+
+RevitTest project to test workaround to create Detail Groups in Revit without ActiveView.
+
+Revit API Forum: https://forums.autodesk.com/t5/revit-api-forum/placing-detail-group-in-legend-view-without-access-to-ui-design/td-p/12792753
+
+## License
+
+This project is [licensed](LICENSE) under the [MIT License](https://en.wikipedia.org/wiki/MIT_License).
+
+---
+
+Do you like this project? Please [star this project on GitHub](../../stargazers)!
---
+++ RevitTestPlaceGroup.Tests/Tests.cs
@@ -0,0 +1,168 @@
+﻿using Autodesk.Revit.DB;
+using NUnit.Framework;
+using RevitTestPlaceGroup.Tests.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RevitTestPlaceGroup.Tests
+{
+    public class Tests : OneTimeOpenDocumentTest
+    {
+        GroupDetailService groupDetailService = new GroupDetailService();
+        protected override string FileName => "Files/ProjectGroupDetail2021.rvt";
+
+        [Test]
+        public void Test_Document_Save()
+        {
+            Assert.IsNotNull(document);
+            Console.WriteLine(document.Title);
+
+            document.SaveAs($"Files/{document.Title}_{application.VersionNumber}.rvt", new SaveAsOptions() { MaximumBackups = 1, OverwriteExistingFile = true });
+        }
+
+        [Test]
+        public void PlaceGroupDetail_ShouldBe_InvalidElementId()
+        {
+            var groupType = groupDetailService.SelectType(document).FirstOrDefault();
+            Assert.IsNotNull(groupType);
+
+            using (Transaction transaction = new Transaction(document))
+            {
+                transaction.Start("PlaceGroup");
+                var group = document.Create.PlaceGroup(new XYZ(0, 0, 0), groupType);
+
+                Assert.IsNotNull(group);
+                Assert.AreEqual(ElementId.InvalidElementId, group.OwnerViewId);
+
+                transaction.Commit();
+            }
+
+        }
+
+        [Test]
+        public void CreateGroupDetail_ShouldBe_ValidElementId()
+        {
+            var viewLegend = SelectViewLegend().FirstOrDefault();
+
+            Assert.IsNotNull(viewLegend);
+
+            var groupTypes = groupDetailService.SelectType(document);
+
+            foreach (var groupType in groupTypes)
+            {
+                using (Transaction transaction = new Transaction(document))
+                {
+                    transaction.Start("PlaceGroup");
+                    var group = groupDetailService.CreateGroupDetail(groupType, viewLegend, new XYZ(0, 0, 0));
+
+                    Assert.IsNotNull(group);
+                    Assert.AreEqual(viewLegend.Id, group.OwnerViewId);
+
+                    transaction.Commit();
+                }
+            }
+        }
+
+        private IEnumerable<View> SelectViewLegend()
+        {
+            return new FilteredElementCollector(document)
+                .OfClass(typeof(View))
+                .OfType<View>()
+                .Where(e => e.ViewType == ViewType.Legend);
+        }
+
+        [Test]
+        public void ShowGroups()
+        {
+            foreach (var groupType in groupDetailService.SelectType(document))
+            {
+                Console.WriteLine(groupType.Name);
+            }
+        }
+
+    }
+
+    public class GroupDetailService
+    {
+        public IEnumerable<GroupType> SelectType(Document document)
+        {
+            var elements = new FilteredElementCollector(document)
+                .OfCategory(BuiltInCategory.OST_IOSDetailGroups)
+                .WhereElementIsElementType()
+                .OfClass(typeof(GroupType))
+                .OfType<GroupType>();
+
+            return elements;
+        }
+
+        public IEnumerable<Group> Select(Document document)
+        {
+            var elements = new FilteredElementCollector(document)
+                .OfCategory(BuiltInCategory.OST_IOSDetailGroups)
+                .WhereElementIsNotElementType()
+                .OfClass(typeof(Group))
+                .OfType<Group>();
+
+            return elements;
+        }
+
+        public Group CreateGroupDetail(GroupType groupType, View view, XYZ location)
+        {
+            var document = groupType.Document;
+
+            Group groupBase = Select(document).FirstOrDefault();
+
+            if (groupBase is null)
+            {
+                throw new Exception("No Detail Group found in the document.");
+            }
+
+            // Console.WriteLine($"CreateGroupDetail using {groupBase.GroupType.Name} [{groupBase.GroupType.Id}] to {groupType.Name} [{groupType.Id}]");
+
+            var groupView = document.GetElement(groupBase.OwnerViewId) as View;
+
+            var elementsToCopy = new[] { groupBase.Id };
+            var options = new CopyPasteOptions();
+            var elements = ElementTransformUtils.CopyElements(groupView, elementsToCopy, view, Transform.Identity, options);
+
+            var newGroup = elements.Select(e => document.GetElement(e)).OfType<Group>().FirstOrDefault();
+            newGroup.ChangeTypeId(groupType.Id);
+
+            var locationPoint = newGroup.Location as LocationPoint;
+            var oldLocation = locationPoint.Point;
+            locationPoint.Point = location;
+
+            // Console.WriteLine($"CreateGroupDetail location {oldLocation} to {locationPoint.Point}");
+
+            return newGroup;
+        }
+
+        private static Group ChangeView(Group group, View newView)
+        {
+            // Model Groups do not have an owner view
+            if (group.OwnerViewId == ElementId.InvalidElementId)
+                return group;
+
+            // Group is already in the correct view
+            if (group.OwnerViewId == newView.Id)
+                return group;
+
+            Console.WriteLine($"ChangeView to {group.OwnerViewId}");
+
+            var document = group.Document;
+            var view = document.GetElement(group.OwnerViewId) as View;
+
+            var elementsToCopy = new[] { group.Id };
+
+            var options = new CopyPasteOptions();
+
+            var elements = ElementTransformUtils.CopyElements(view, elementsToCopy, newView, Transform.Identity, options);
+            document.Delete(elementsToCopy);
+
+            var newGroup = elements.Select(e => document.GetElement(e)).OfType<Group>().FirstOrDefault();
+            return newGroup;
+        }
+
+    }
+}
